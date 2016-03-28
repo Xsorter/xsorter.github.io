234f8b06df9da8ca853d97836c0fad7f3025eeeb
Oleksii Nelin <Xsorter@gmail.com>
2016-03-29 00:41:03 +0300

    G&T jquery added to menu

--- GlTar/b2.01/js/jqscript.js
+++ GlTar/b2.01/js/jqscript.js
@@ -2,6 +2,6 @@ $(function(){
 	var $navicon = $('.header-navicon');
 	$navicon.on('click', function(){
 		var $dropdown = $('.header-navigation-menu-mobile');
-		$dropdown.slideToggle(700);
+		$dropdown.slideToggle();
 	});
 });
