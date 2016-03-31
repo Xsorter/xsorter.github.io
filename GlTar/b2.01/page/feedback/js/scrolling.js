01f6aedfebbd0112b9991920bc71295ae27aaa6b
Oleksii Nelin <Xsorter@gmail.com>
2016-03-31 21:59:42 +0300

    G&T code fixed, minimized

--- GlTar/b2.01/page/feedback/js/scrolling.js
+++ GlTar/b2.01/page/feedback/js/scrolling.js
@@ -6,30 +6,21 @@ $(function () {
         body = $.browser.webkit ? $('body') : $('html'),
         wheel = false;
  
- 
     $('body').mousewheel(function(event, delta) {
- 
         wheel = true;
- 
         if (delta < 0) {
- 
             top = (top+viewport) >= $(document).height() ? top : top+=step;
- 
             body.stop().animate({scrollTop: top}, 400, function () {
                 wheel = false;
             });
         } else {
- 
             top = top <= 0 ? 0 : top-=step;
- 
             body.stop().animate({scrollTop: top}, 400, function () {
                 wheel = false;
             });
         }
- 
         return false;
     });
- 
     $(window).on('resize', function (e) {
         viewport = $(this).height();
     });
