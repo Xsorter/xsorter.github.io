7fc5ee6708f9bee034d158119f3ddf432965676c
Aleksey Nelin <xsorter@gmail.com>
2016-05-14 01:27:08 +0300

    task 15 added JS

--- task15-16/js/script.js
+++ task15-16/js/script.js
@@ -1,16 +1,15 @@
 function GoogleCallback(data, textStatus){
     console.log(data);
-    
+
     if (parseInt(data.totalHits) > 0)
         $.each(data.hits, function(i, hit){ //find object and each hit append to list
-        var result = hit.webformatURL;
-        $('#list').append("<li>"+"<img src="+result+">"); 
+            var pageURL = hit.pageURL;
+            var result = hit.webformatURL;
+            $('#list').append("<li>"+"<img src="+result+ ">");              
     });
     else
         alert('Картинок с таким названием не найдено');
-        console.log('No hits');
 };        
-
 
 $(function(){
     $('#submit').click(function(e){ 
@@ -21,8 +20,6 @@ $(function(){
         url:'https://pixabay.com/api/?key=2566046-80b4e8851317db138039b9381&q='+query+'&callback=GoogleCallback&context=?',
         dataType: 'jsonp',
         method: 'GET',
-    
-
         });   
     });
 });
