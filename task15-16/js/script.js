8805c5e1ff9a3f1f43053caca552d61f9e460189
Aleksey Nelin <xsorter@gmail.com>
2016-05-14 20:33:34 +0300

    task 15-16 added JS

--- task15-16/js/script.js
+++ task15-16/js/script.js
@@ -23,3 +23,61 @@ $(function(){
         });   
     });
 });
+
+
+//prototypes
+
+function Human()  {
+    this.name = 'alex',
+    this.age = 28,
+    this.sex = 'male',
+    this.height = 180, 
+    this.weight = 85
+};
+
+console.log(Human);
+
+Worker.prototype = new Human();
+Student.prototype = new Human();
+
+function Worker()  {
+    this.job = 'office',
+    this.salary = 10000,
+    this.working = function (){
+        console.log('i am working and this is work method')
+    };
+};
+
+function Student()  {
+    this.university = 'DNU',
+    this.money = 500,
+    this.watching = function (){
+        console.log('i am watching TV show and this is watching method')
+    };
+};
+
+
+
+var worker1 = new Worker();
+var worker2 = new Worker();
+var worker3 = new Worker();
+var worker4 = new Worker();
+var worker5 = new Worker();
+
+console.log(worker1.salary);
+console.log(worker2.name);
+console.log(worker3.working());
+console.log(worker4.job);
+console.log(worker5.sex);
+
+var student1 = new Student();
+var student2 = new Student();
+var student3 = new Student();
+var student4 = new Student();
+var student5 = new Student();
+
+console.log(student1.university);
+console.log(student2.name);
+console.log(student3.watching());
+console.log(student4.sex);
+console.log(student5.height);
