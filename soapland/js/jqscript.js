97bff8aa1bb90d6060fab0850a5f8466b916c9c7
Aleksey Nelin <xsorter@gmail.com>
2016-05-22 08:03:01 +0300

    soapland mobile ver beta

--- soapland/js/jqscript.js
+++ soapland/js/jqscript.js
@@ -2,7 +2,7 @@ $(function(){
 	var $navicon = $('.header-navicon');
 	$navicon.on('click', function(){
 		var $dropdown = $('.header-menu-mobile');
-		$dropdown.slideToggle("slow");	
+		$dropdown.slideToggle("fast");	
 		//$dropdown.slideToggle();
 	});
 });
