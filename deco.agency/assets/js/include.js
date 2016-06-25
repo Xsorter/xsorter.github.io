aed81919f9d8140cd01f6b4c98afe0f575648b41
Xsorter Nelin <Xsorter Nelin>
2016-06-25 03:28:26 +0300

    deco.agency JS done

--- deco.agency/assets/js/include.js
+++ deco.agency/assets/js/include.js
@@ -1,6 +1,21 @@
+$(function(){
+	
+	$('.focus-document').on('mouseover', function(){
+		$('.hover-document').css("visibility", "hidden");
+		$('.modal-document').css("visibility", "visible");	
+	});
+	$('.focus-document').on('mouseleave', function(){
+		$('.hover-document').css("visibility", "visible");
+		$('.modal-document').css("visibility", "hidden");	
+	});
 
-jQuery(document).ready(function($){
-
-
+	$('.focus-optimization').on('mouseover', function(){
+		$('.hover-optimization').css("visibility", "hidden");
+		$('.modal-optimization').css("visibility", "visible");	
+	});
+	$('.focus-optimization').on('mouseleave', function(){
+		$('.hover-optimization').css("visibility", "visible");
+		$('.modal-optimization').css("visibility", "hidden");	
+	});
 
 });
